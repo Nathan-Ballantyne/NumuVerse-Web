f0da302e7e094ad7118300800c08ab1cfed20f05
Nathan Ballantyne <nathan.ballantyne@gmail.com>
2021-10-24 18:49:25 +1300

    move header styles to external css file

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,18 +1,11 @@
 import Image from 'next/image';
 import SocialLinks from '../SocialLinks';
-// import { Container, Row, Col } from 'react-bootstrap';
 import headerImage from '../../public/LandingPage.png';
 import styles from './Header.module.css';
 
 const Header = () => {
   return (
-    <div
-      style={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'end',
-      }}
-    >
+    <div className={styles.socialLinks}>
       <SocialLinks />
 
       <Image
