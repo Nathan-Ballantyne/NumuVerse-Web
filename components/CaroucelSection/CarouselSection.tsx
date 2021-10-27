e8ad2a6c6a4318e1c813e1a9b59fc8bf87d5d98e
Nathan Ballantyne <nathan.ballantyne@gmail.com>
2021-10-28 11:34:39 +1300

    add murky hollow swamp environment image

--- components/CaroucelSection/CarouselSection.tsx
+++ components/CaroucelSection/CarouselSection.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import { Carousel } from 'react-bootstrap';
 import coralEnv from '../../public/OG_Coral_001.png';
 import desertEnv from '../../public/OG_Desert_026.png';
+import swampEnv from '../../public/Environment_Swamp_01.png';
 
 const CaroucelSection = () => {
   return (
@@ -11,6 +12,9 @@ const CaroucelSection = () => {
       </Carousel.Item>
       <Carousel.Item interval={2000}>
         <Image src={desertEnv} alt='desert environment' placeholder='blur' />
+      </Carousel.Item>
+      <Carousel.Item interval={2000}>
+        <Image src={swampEnv} alt='swamp environment' placeholder='blur' />
       </Carousel.Item>
     </Carousel>
   );
